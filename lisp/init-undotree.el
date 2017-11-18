ecf0f99426a90223e6a40f4505c17927ddb2aadb
lihong <kettyleem@163.com>
2017-11-18 21:09:53 +0800

    remove undotree, Because it has been

--- lisp/init-undotree.el
+++
@@ -1,9 +0,0 @@
-;; -*- coding: utf-8 -*-
-
-;; undo tree mode
-(require-package 'undo-tree)
-(require 'undo-tree)
-(setq undo-tree-mode-lighter " UT")
-(global-undo-tree-mode)
-(global-set-key (kbd "M-/") 'undo-tree-undo)
-(provide 'init-undotree)
