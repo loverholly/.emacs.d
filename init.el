1867133c95fe6ec86d8c35683ac34f5581d5efab
Steve Purcell <steve@sanityinc.com>
2018-01-27 15:04:42 +1300

    Move `uptimes` earlier in init.el

--- init.el
+++ init.el
@@ -130,6 +130,10 @@ Return the updated `exec-path'"
 (maybe-require-package 'regex-tool)
 (maybe-require-package 'dotenv-mode)
 
+(when (maybe-require-package 'uptimes)
+  (add-hook 'after-init-hook (lambda () (require 'uptimes))))
+
+
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
 ;;----------------------------------------------------------------------------
@@ -157,8 +161,6 @@ Return the updated `exec-path'"
 (require 'init-locales)
 
 
-(when (maybe-require-package 'uptimes)
-  (add-hook 'after-init-hook (lambda () (require 'uptimes))))
 
 
 (provide 'init)
