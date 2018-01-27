11e64eacae654d0d22f3679e1d1b29391d8f79c3
Steve Purcell <steve@sanityinc.com>
2018-01-27 15:04:56 +1300

    Place loading of init-local after init-locales, to allow overrides
    See #320

--- init.el
+++ init.el
@@ -150,16 +150,15 @@ Return the updated `exec-path'"
 
 
 ;;----------------------------------------------------------------------------
-;; Allow users to provide an optional "init-local" containing personal settings
-;;----------------------------------------------------------------------------
-(require 'init-local nil t)
-
-
-;;----------------------------------------------------------------------------
 ;; Locales (setting them earlier in this file doesn't work in X)
 ;;----------------------------------------------------------------------------
 (require 'init-locales)
 
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(require 'init-local nil t)
 
 
 
