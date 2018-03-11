72759528340c8af3e72782fd53ca939c4f22f21f
Steve Purcell <steve@sanityinc.com>
2018-03-11 14:13:32 +1300

    Better override M-? in tagedit-mode (see #550)

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -4,9 +4,6 @@
   (define-key tagedit-mode-map (kbd "M-?") nil)
   (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
-(after-load 'tagedit
-  (define-key tagedit-mode-map (kbd "M-?") nil))
-
 (add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
 
 ;; Note: ERB is configured in init-ruby
