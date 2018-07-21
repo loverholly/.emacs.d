67d071ee9fc174542ba6f6439a3ad4cf801b696e
lihong <kettyleem@163.com>
2018-07-21 21:48:39 +0800

    fixed:format-command redefined by init-cppcheck.el

--- lisp/init-cppcheck.el
+++ lisp/init-cppcheck.el
@@ -1,13 +1,13 @@
-(setq format-command (format "cppcheck --enable=all"))
+(setq cppcheck-command (format "cppcheck --enable=all"))
 
 (global-auto-revert-mode t)
 
 (defun cppcheck-code ()
-  "Format current buffer"
+  "Cppcheck current buffer"
   (interactive)
   (let ((file (buffer-file-name)))
 	(save-excursion
-	  (async-shell-command (format "%s %s" format-command file))
+	  (async-shell-command (format "%s %s" cppcheck-command file))
 	  (message "cppchecked over"))))
 
 (global-set-key  (kbd "C-c k") 'cppcheck-code)
