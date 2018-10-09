a993589b09a57114b9990055c0e6d897a8445377
lihong <kettyleem@163.com>
2018-10-09 19:11:28 +0800

    feat:init-xterm.el:add multi shell

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -9,7 +9,11 @@
   (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
   (mwheel-install))
 
-
+;; set open multi shell
+(defun wcy-shell-mode-auto-rename-buffer (text)
+  (if (eq major-mode 'shell-mode)
+	  (rename-buffer  (concat "shell:" default-directory) t)))
+(add-hook 'comint-output-filter-functions'wcy-shell-mode-auto-rename-buffer)
 
 (add-hook 'after-make-console-frame-hooks 'sanityinc/console-frame-setup)
 
