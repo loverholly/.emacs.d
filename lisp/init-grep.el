a3d0f20ca17a962fec29e52fbd5d51fcb16e487f
lihong <kettyleem@163.com>
2018-10-21 17:51:52 +0800

    add grep for whole word pattern, and color display

--- lisp/init-grep.el
+++ lisp/init-grep.el
@@ -3,7 +3,7 @@
 ;;; redefine the default grep template;some times emacs for windows the arguments is wrong
 ;; (setq-default grep-find-template '[grep -nH --null "{}" ";"])
 (setq-default grep-use-null-device nil)
-(setq-default grep-command "grep  -nH ")
+(setq-default grep-command "grep  -nwH --color ")
 
 (when *is-a-mac*
   (setq-default locate-command "mdfind"))
