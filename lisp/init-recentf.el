e06afb7804a214b1bc73d1968b459a6134463a3a
lihong <kettyleem@163.com>
2019-04-06 21:28:42 +0800

    feat:add init file for key f2

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -7,6 +7,11 @@
  recentf-max-saved-items 1000
  recentf-exclude '("/tmp/" "/ssh:"))
 
+(defun open-emacs-init-file ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-emacs-init-file)
 
 (provide 'init-recentf)
 ;;; init-recentf.el ends here
