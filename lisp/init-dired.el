553a2b50d0f11eedf598b5252280b46437929a38
lihong <kettyleem@163.com>
2019-04-06 23:01:59 +0800

    fix:init-dired.el:adjust dired mode for one buffer

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -21,5 +21,11 @@
   (after-load 'dired
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
 
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+(put 'dired-find-alternate-file 'disabled nil)
+(with-eval-after-load 'dired
+  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+
 (provide 'init-dired)
 ;;; init-dired.el ends here
