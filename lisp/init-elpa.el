e03f9a64b126028406bb92724f87eb4a4d624068
lihong <kettyleem@163.com>
2019-08-04 11:26:54 +0800

    add melpa mirrior for melpa

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -20,7 +20,7 @@
        (proto (if no-ssl "http" "https")))
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
   ;; Official MELPA Mirror, in case necessary.
-  ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
+  (add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")) t)
   (if (< emacs-major-version 24)
       ;; For important compatibility libraries like cl-lib
       (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))
