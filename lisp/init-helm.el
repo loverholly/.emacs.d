323c1c03cd9cb878612dc1a6209acfea071f662a
lihong <kettyleem@163.com>
2019-11-16 12:28:51 +0800

    delete config for helm

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -132,7 +132,4 @@
 (define-key helm-gtags-mode-map (kbd "C-c C-g") 'helm-gtags-create-tags)
 (define-key helm-gtags-mode-map (kbd "C-c C-j") 'helm-gtags-find-symbol)
 
-;; save symbol auto update
-(setq helm-gtags-auto-update t) 
-
 (provide 'init-helm)
