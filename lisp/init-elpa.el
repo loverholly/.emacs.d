a6a67a0f6f0041a00e077797b7198350cf534924
lihong <kettyleem@163.com>
2020-08-14 22:18:01 +0800

    format el file
    Signed-off-by: lihong <kettyleem@163.com>

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -4,7 +4,6 @@
 
 (require 'package)
 
-
 ;;; Install into separate package dirs for each Emacs version, to prevent bytecode incompatibility
 (let ((versioned-package-dir
        (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
@@ -12,7 +11,6 @@
   (setq package-user-dir versioned-package-dir))
 
 
-
 ;;; Standard package repositories
 
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
@@ -33,7 +31,6 @@
 (when (and (version< emacs-version "26.3") (boundp 'libgnutls-version) (>= libgnutls-version 30604))
   (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 
-
 ;;; On-demand installation of packages
 
 (require 'cl-lib)
@@ -64,13 +61,11 @@ locate PACKAGE."
      (message "Couldn't install optional package `%s': %S" package err)
      nil)))
 
-
 ;;; Fire up package.el
 
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-
 ;; package.el updates the saved version of package-selected-packages correctly only
 ;; after custom-file has been loaded, which is a bug. We work around this by adding
 ;; the required packages to package-selected-packages after startup is complete.
@@ -92,15 +87,12 @@ locate PACKAGE."
             (lambda () (package--save-selected-packages
                    (seq-uniq (append sanityinc/required-packages package-selected-packages))))))
 
-
 (require-package 'fullframe)
 (fullframe list-packages quit-window)
 
-
 (let ((package-check-signature nil))
   (require-package 'gnu-elpa-keyring-update))
 
-
 (defun sanityinc/set-tabulated-list-column-width (col-name width)
   "Set any column with name COL-NAME to the given WIDTH."
   (when (> width (length col-name))
