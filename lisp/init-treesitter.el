9157fa5ac0d8f01310c374b16217127209ef1436
johenleem <kettyleem@163.com>
2023-11-09 20:27:09 +0800

    fix c/c++ ts mode indent
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-treesitter.el
+++ lisp/init-treesitter.el
@@ -13,6 +13,29 @@
 (add-hook 'c-mode #'tree-sitter-hl-mode)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
+(defun my-indent-style()
+  "Override the built-in BSD indentation style with some additional rules"
+  `(;; Here are your custom rules
+    ((node-is ")") parent-bol 0)
+    ((match nil "argument_list" nil 1 1) parent-bol c-ts-mode-indent-offset)
+    ((parent-is "argument_list") prev-sibling 0)
+    ((match nil "parameter_list" nil 1 1) parent-bol c-ts-mode-indent-offset)
+    ((parent-is "parameter_list") prev-sibling 0)
+
+    ;; Append here the indent style you want as base
+    ,@(alist-get 'bsd (c-ts-mode--indent-styles 'cpp))))
+
+(use-package c-ts-mode
+  :if (treesit-language-available-p 'c)
+  :custom
+  (c-ts-mode-indent-offset 8)
+  (c-ts-mode-indent-style #'my-indent-style)
+  :init
+  ;; Remap the standard C/C++ modes
+  (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c-or-c++-mode . c-or-c++-ts-mode)))
+
 (setq tree-sitter-hl-mode t)
 
 (setq treesit-language-source-alist
