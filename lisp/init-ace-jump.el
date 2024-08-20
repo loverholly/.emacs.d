fdcd570da61f41e77fdfa8dd217a98b43d8f8409
hongli <hong.li@innogritcorp.com>
2024-08-20 21:37:41 +0800

    add ace jump mode
    Signed-off-by: hongli <hong.li@innogritcorp.com>

--- lisp/init-ace-jump.el
+++ lisp/init-ace-jump.el
@@ -1,6 +1,8 @@
-;;; init-ace-jump.el --- ace-jump support -*- lexical-binding: t -*-
+;;; init-ace-jump.el --- ace-jump-mode support -*- lexical-binding: t -*-
 ;;; Commentary:
 
 (require-package 'ace-jump-mode)
+(ace-jump-mode t)
+(global-set-key (kbd "C-x o") 'ace-jump-mode)
 
 (provide 'init-ace-jump)
