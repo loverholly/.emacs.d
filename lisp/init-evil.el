25eebd27447f5b7c8f31dcf03b29dc99ca7d841c
johenleem <kettyleem@163.com>
2024-08-24 07:37:27 +0800

    enable evil visual mark mode
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -2,8 +2,11 @@
 ;;; Commentary:
 ;;; Code:
 (require-package 'evil)
+(require-package 'evil-visual-mark-mode)
+(require-package 'evil-visual-replace)
 
 (evil-mode 1)
+(evil-visual-mark-mode 1)
 
 (provide 'init-evil)
 ;;; init-evil.el ends here
