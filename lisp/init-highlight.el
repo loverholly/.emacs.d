063ba6d86b67c236d1547825f83056318df73148
johenleem <kettyleem@163.com>
2024-09-01 20:45:56 +0800

    add the highlight parser mode
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -2,8 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 (require-package 'highlight-symbol)
+(require-package 'highlight-parentheses)
 
 (global-set-key (kbd "<f3>") 'highlight-symbol)
+(highlight-parentheses-mode 1)
 
 (provide 'init-highlight)
 ;;; init-highlight.el ends here
