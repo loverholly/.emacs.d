7e2378f2ad84eb5860f6de784900079ec21bffdd
hongli <hong.li@innogritcorp.com>
2024-09-02 10:23:17 +0800

    enable yasnippet for c/c++ code
    Signed-off-by: hongli <hong.li@innogritcorp.com>

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -1,7 +1,10 @@
 ;; -*- coding: utf-8 -*-
 ;; yasnippet
 (require-package 'yasnippet)
-(require 'yasnippet)
+(require-package 'helm-c-yasnippet)
+(require-package 'yasnippet-capf)
+(require-package 'yasnippet-snippets)
+
 (yas-global-mode 1)
 
 (provide 'init-yasnippet)
