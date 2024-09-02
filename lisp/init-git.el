1c3070de6a45e2d69e0f64ac4ab45162dc55bc84
hongli <hong.li@innogritcorp.com>
2024-09-02 11:04:27 +0800

    disable the git-commit for conflict with magit
    Signed-off-by: hongli <hong.li@innogritcorp.com>

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -47,8 +47,8 @@
 (with-eval-after-load 'magit
   (fullframe magit-status magit-mode-quit-window))
 
-(when (maybe-require-package 'git-commit)
-  (add-hook 'git-commit-mode-hook 'goto-address-mode))
+;;(when (maybe-require-package 'git-commit)
+;;  (add-hook 'git-commit-mode-hook 'goto-address-mode))
 
 
 ;; Convenient binding for vc-git-grep
