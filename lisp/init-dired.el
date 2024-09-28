89099ee9bc1f0dfa4197bba5f0e8a636edf9f8b0
johenleem <kettyleem@163.com>
2024-09-28 21:17:11 +0800

    add the dired config
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -2,6 +2,26 @@
 ;;; Commentary:
 ;;; Code:
 
+(require-package 'dired-subtree)
+(require-package 'dired-rainbow)
+(require-package 'dired-single)
+(require-package 'dired-rsync)
+(require-package 'dired-sidebar)
+(require-package 'ibuffer-sidebar)
+
+(global-set-key (kbd "<f4>") 'dired-sidebar-show-sidebar)
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+(add-hook 'dired-mode-hook 'auto-revert-mode)
+
+(defun revert-dired-on-find-file ()
+  "Revert Dired buffer when a file is opened."
+  (when (eq major-mode 'dired-mode)
+    (revert-buffer)))
+
+(add-hook 'find-file-hook 'revert-dired-on-find-file)
+
+
 (setq-default dired-dwim-target t)
 
 ;; Prefer g-prefixed coreutils version of standard utilities when available
