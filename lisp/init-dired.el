a83ca19e6f3126a036fcc993de1fdd96cbbe84be
johenleem <kettyleem@163.com>
2024-10-07 20:22:31 +0800

    update the dired for recent file
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -49,5 +49,21 @@
   (with-eval-after-load 'dired
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
 
+(defun recentd-track-opened-file ()
+  "Insert the name of the directory just opened into the recent list."
+  (and (derived-mode-p 'dired-mode) default-directory
+       (recentf-add-file default-directory))
+  ;; Must return nil because it is run from `write-file-functions'.
+  nil)
+
+(defun recentd-track-closed-file ()
+  "Update the recent list when a dired buffer is killed.
+That is, remove a non kept dired from the recent list."
+  (and (derived-mode-p 'dired-mode) default-directory
+       (recentf-remove-if-non-kept default-directory)))
+
+(add-hook 'dired-after-readin-hook 'recentd-track-opened-file)
+(add-hook 'kill-buffer-hook 'recentd-track-closed-file)
+
 (provide 'init-dired)
 ;;; init-dired.el ends here
