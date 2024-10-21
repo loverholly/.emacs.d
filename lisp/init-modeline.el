41172a9f1fb130649cc67b5d49588d4f354f55f6
johenleem <kettyleem@163.com>
2024-10-21 21:23:43 +0800

    set sml theme is dark
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -3,8 +3,7 @@
 ;;; Code:
 (require-package 'smart-mode-line)
 
-(smart-mode-line-enable t)
-(set-face-background 'mode-line "#000000") ; 设置活动模式行的背景颜色为黑色
-(set-face-background 'mode-line-inactive "#1F1F1F") ; 设置非活动模式行的背景颜色为深灰色
+(setq sml/theme 'respectful)
+(sml/setup)
 
 (provide 'init-modeline)
