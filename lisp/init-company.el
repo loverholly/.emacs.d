538b247a03629d9695756684002a4b4a90d8bd27
kettyleem <kettyleem@163.com>
2024-10-31 17:01:51 +0800

    add the company backend completion symbol support
    Signed-off-by: kettyleem <kettyleem@163.com>

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -8,6 +8,11 @@
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
+(use-package company-irony
+  :ensure t
+  :config
+  (add-to-list 'company-backends 'company-irony))
+
 (when (maybe-require-package 'company)
   (add-hook 'after-init-hook 'global-company-mode)
   (with-eval-after-load 'company
@@ -20,11 +25,21 @@
     (define-key company-active-map (kbd "C-d") 'company-show-doc-buffer)
     (define-key company-active-map (kbd "M-.") 'company-show-location)
     (setq-default company-dabbrev-other-buffers 'all
-                  company-tooltip-align-annotations t))
+                  company-tooltip-align-annotations t
+		  company-minimum-prefix-length 3
+		  company-show-numbers t
+		  company-idle-delay 0.0
+		  company-selection-wrap-around t
+		  company-transformers '(company-sort-by-backend-importance)
+		  company-backends '((company-clang company-dabbrev-code))))
   (global-set-key (kbd "M-C-/") 'company-complete)
   (when (maybe-require-package 'company-quickhelp)
     (add-hook 'after-init-hook 'company-quickhelp-mode)))
 
+(add-hook 'c-ts-mode-hook 'company-mode)
+(add-hook 'c-mode-hook 'company-mode)
+(add-hook 'c++-mode-hook 'company-mode)
+(add-hook 'c++-ts-mode 'company-mode)
 
 (provide 'init-company)
 ;;; init-company.el ends here
