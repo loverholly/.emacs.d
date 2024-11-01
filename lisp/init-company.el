8f314bd1eced36fd6baa85815607db0834c057ac
johenleem <kettyleem@163.com>
2024-11-01 20:53:59 +0800

    use the clang to completion backend
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -8,11 +8,6 @@
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
-(use-package company-irony
-  :ensure t
-  :config
-  (add-to-list 'company-backends 'company-irony))
-
 (when (maybe-require-package 'company)
   (add-hook 'after-init-hook 'global-company-mode)
   (with-eval-after-load 'company
