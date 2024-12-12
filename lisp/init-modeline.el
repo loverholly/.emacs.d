94bda542d97b699e3b5749993d9293493aa995fd
johenleem <kettyleem@163.com>
2024-12-12 21:56:17 +0800

    update the sml theme setting
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -4,9 +4,9 @@
 (use-package smart-mode-line
   :ensure t
   :init
+  (setq sml/theme 'respectful)
   (sml/setup)
   :config
-  (setq sml/theme 'respectful)
   (setq sml/vc t)
   (setq sml/vc-mode-show-backend t)
   (setq keycast-mode-line-insert-after 'mode-line-end-spaces))
