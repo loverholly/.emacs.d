18edf24eccf4eb133b5a80629fc4413164b1b0f1
johenleem <kettyleem@163.com>
2024-12-18 22:13:23 +0800

    update the csv config
    Signed-off-by: johenleem <kettyleem@163.com>

--- lisp/init-csv.el
+++ lisp/init-csv.el
@@ -5,7 +5,8 @@
 (when (maybe-require-package 'csv-mode)
   (add-auto-mode 'csv-mode "\\.[Cc][Ss][Vv]\\'")
 
-  (setq csv-separators '("," ";" "|" " ")))
+  (setq csv-separators '("," ";" "|" " "))
+  (add-hook 'csv-mode-hook 'csv-align-mode))
 
 (provide 'init-csv)
 ;;; init-csv.el ends here
